21c83bc23dc7e96acaa6d64cafb43fc97294f268
puang59 <kk61468@gmail.com>
2024-12-16 14:56:29 +0530

    feat: drizzle export

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,2 +1,19 @@
 export * from "./database";
 export * from "./schema";
+export {
+  eq,
+  and,
+  or,
+  not,
+  gt,
+  gte,
+  lt,
+  lte,
+  like,
+  ilike,
+  between,
+  isNull,
+  isNotNull,
+  inArray,
+  notInArray,
+} from "drizzle-orm";
